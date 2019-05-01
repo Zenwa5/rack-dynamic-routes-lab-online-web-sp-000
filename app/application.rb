23a9e5ae4d49a4752d2fe0c730a489c97ebadaef
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-05-01 02:19:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,6 +5,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path=="/items/<ITEM NAME>"
+      resp.write "price"
     else
      resp.status = 400
      resp.write "Route not found"
