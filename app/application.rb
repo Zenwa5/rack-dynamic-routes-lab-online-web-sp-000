e62c1a0cc0c83a43da891367d46f69b68eadfb7a
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-05-01 02:26:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,12 +6,19 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items/<ITEM NAME>"
-      resp.write "price"
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = @@items.find {|i| i.name == item_name}
+      if item
+        resp.write item.price
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
     else
-     resp.status = 400
-     resp.write "Route not found"
-
+      resp.write "Route not found"
+      resp.status = 404
+    end
    end
  end
 
